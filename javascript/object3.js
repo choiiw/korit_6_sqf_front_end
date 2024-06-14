87d1dc8a6df91be02f4cbed585d307a6f1c6bdf4
choiiw <tjddnjs5245@naver.com>
2024-06-14 17:02:34 +0900

    6/14 -f

---
+++ javascript/object3.js
@@ -0,0 +1,32 @@
+function modifyUser(user, target, value) {
+    const newUser = {
+        ...user,
+        [target]: value
+    };
+    return newUser;
+}
+
+
+function main() {
+    let user = {
+        username: "admin",
+        password: "1234"
+    }
+
+    console.log(user);
+
+    const newUser = modifyUser(user, "username", "test-user");
+    console.log(newUser);
+
+    const newUser2 = modifyUser(newUser, "password", "1111")
+    console.log(newUser2);
+
+    const userList = [user, newUser];
+    const newUserList = [ ...userList, newUser2 ]; 
+    
+    // 스프레드 -> 깊은 복사 
+
+    const userList2 = userList; // 얕은 복사  
+
+}
+main();
---
+++ javascript/object4.js
@@ -0,0 +1,29 @@
+ // 비구조 할당 or 구조 분해 
+ function main() {
+    const user = {
+        username: "admin",
+        password: "1234",
+        name: "김준일",
+        email: "aaa@gmail.com"
+    }
+
+    const names = [ "박현주", "이성희", "권오광", "권혁진"];
+
+    const { username, password, email } = user;
+    
+    console.log(username); 
+    console.log(password); 
+    console.log(email); 
+
+    const [ a, b ] = names;
+
+    console.log(a);
+    console.log(b);
+
+    const { name, ...rest } = user;
+    console.log(rest); 
+
+    console.log(names.slice(1, 3));
+ }
+
+ main();
