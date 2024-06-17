016845419d9f6bbedf7aa9ec32ab003b626bc1a2
choiiw <tjddnjs5245@naver.com>
2024-06-17 16:38:37 +0900

    6/17

---
+++ javascript/filter.js
@@ -0,0 +1,9 @@
+let names = ["김준일", "김준이", "김준삼"];
+
+console.log(names);
+
+names = names.map(name => {return name + "님"})
+console.log(names);
+
+names = names.filter(( name , index ) => index !== 1);
+console.log(names);
--- javascript/user_management.js
+++ javascript/user_management.js
@@ -1,55 +1,99 @@
-const list = ["e","r","t"];
-console.log(list.join("s"));
-
 let userList = [];
+loadUserList()
+
 let emptyUser = {
+    id: 0,
+    name: "",
     username: "",
     password: ""
 };
 
 let user = {
     ...emptyUser
-}
+};
 
 function renderTable() {
     const userTableBody = document.querySelector(".user-table-body");
-    userTableBody.innerHTML = userList.map(({username, password}, index) => {
+    userTableBody.innerHTML = userList.map(({id, name, username, password}, index) => {
         return `
             <tr>
                 <td>${index + 1}</td>
+                <td>${id}</td>
+                <td>${name}</td>
                 <td>${username}</td>
                 <td>${password}</td>
+                <th><button onclick="deleteUser(event)" value="${id}">삭제</button></th>
             </tr>
         `;
     }).join("");
 }
+
+
 
 function handleUserInputKeyDown(e) {
     user = {
         ...user,
         [e.target.name]: e.target.value
     }
+    
+
 
     console.log(user);
 
     if(e.keyCode === 13) {
+    
+        const nameInput = document.querySelector(".name-input");    
         const passwordInput = document.querySelector(".password-input");
         const usernameInput = document.querySelector(".username-input");
-        
-        if(e.target.name === "username") {
-            passwordInput.focus();
-        }
-        if(e.target.name === "password") {
-            userList = [ ...userList, { ...user } ];
 
+        
+        if(e.target.name === "name") {
+            usernameInput.focus();
+        }
+
+
+        if(e.target.name === "username") {
+            passwordInput.focus();           
+        }
+        
+        if(e.target.name === "password") {
+            userList = [ ...userList, { ...user, id: getNewId() } ];
+            
+            saveUserList();
             renderTable();
 
+            nameInput.value = emptyUser.name;
             usernameInput.value = emptyUser.username;
             passwordInput.value = emptyUser.password;
             
-            usernameInput.focus();
+            nameInput.focus();
+            
         }
+        
+
     }
-    console.log(e.target.name);
+}
+    
+ function saveUserList() {
+     localStorage.setItem("userList", JSON.stringify(userList));
+}
+
+function loadUserList() {
+    const lsUserList = localStorage.getItem("userList");
+    userList = !lsUserList ? [] : JSON.parse(lsUserList);
+    renderTable();
+}
+
+function deleteUser(e) {
+    userList = userList.filter(({id}) => id !== parseInt(e.target.value));
+    saveUserList();
+    renderTable();
+}
+
+function getNewId() {
+   const userIds = userList.map(user => user.id);
+   const maxUserId = userIds.length === 0 ? 20240000 : Math.max.apply(null, userIds);
+
+   return maxUserId + 1; 
 }
 
